efd16e9b73820ce3bd2b002f8bf83b1165c1f1d9
Henry Keyser <hwkeyser@gmail.com>
2019-08-03 14:20:33 -0400

    new lift scripts in progress

--- GMTK_2019/Assets/Scripts/Controls.cs
+++ GMTK_2019/Assets/Scripts/Controls.cs
@@ -6,6 +6,7 @@ public class Controls : MonoBehaviour
 {
     // it may make more sense to do "[SerializeField]" as i dont think this needs to be called from anywhere. we just need to edit the feilds from 
 
+    public float startSpeed;        // Start speed for scenario
     public float maxSpeed;          // Our maximum speed allowed.
     public float decAltSpeed;       // Decrease Altitude.
     public float incAltSpeed;       // Increase Altitude.
@@ -13,18 +14,20 @@ public class Controls : MonoBehaviour
     public float thrust;            // How much force to apply (Forward).
     public float airBrakeForce;     // Breaks.
     public float bank;
+    public float liftMultplier;
+    public float lift;
+    public float gravityOverride;
 
     private Rigidbody rb;           // Our Rigidbody.
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();        // Gets the players Rigidbody.
+        rb.velocity = new Vector3(0, 0, startSpeed);
     }
 
     void FixedUpdate()
     {
-
-        currentSpeed = rb.velocity.z;
 
         if (currentSpeed >= maxSpeed)
         {
@@ -38,25 +41,18 @@ public class Controls : MonoBehaviour
         {
             //Apply Accelleration
             rb.AddRelativeForce(Vector3.forward * thrust);
-            if (currentSpeed >= incAltSpeed)
-            {
-                rb.AddRelativeForce(Vector3.up * 10);
-            }
 
         }
         if (Input.GetKey(KeyCode.S))
         {
             //Apply airBrakeForce
             rb.AddRelativeForce(Vector3.back * airBrakeForce);
-            if (currentSpeed <= decAltSpeed)
-            {
-                rb.AddRelativeForce(Vector3.down * 10);
-            }
+            if (currentSpeed <= decAltSpeed);
         }
 
     }
 
-
+    
     void Update()
     {
         //NOT REALISTIC ROTATIONS but not trying to be REALISTIC at this stage.
