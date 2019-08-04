9d1b3e5112d7fc5aba80847bee7772f71dc5f765
DavidLee <changshuen.lee@gmail.com>
2019-08-04 00:51:16 -0400

    David's Scenes

---
+++ GMTK_2019/Assets/cameraShake.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraShake : MonoBehaviour
+{
+    public float waitTime = .01f;
+    private GameObject Camera;
+    public float rotateSpeed = 10f;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Camera = GameObject.FindWithTag("MainCamera");
+        StartCoroutine("translate");
+        StartCoroutine("rotate");
+    }
+
+    IEnumerator translate()
+    {
+        Camera.transform.position += new Vector3(0, .3f, 0); 
+        yield return new WaitForSecondsRealtime(waitTime);
+        Camera.transform.position += new Vector3(0, .2f, 0);
+        yield return new WaitForSecondsRealtime(waitTime);
+        Camera.transform.position += new Vector3(0, .1f, 0);
+        yield return new WaitForSecondsRealtime(waitTime);
+        Camera.transform.position += new Vector3(0, -.1f, 0);
+        yield return new WaitForSecondsRealtime(waitTime);
+        Camera.transform.position += new Vector3(0, -.2f, 0);
+        yield return new WaitForSecondsRealtime(waitTime);
+        Camera.transform.position += new Vector3(0, -.3f, 0);
+        yield return new WaitForSecondsRealtime(waitTime);
+
+        Camera.transform.position += new Vector3(0, .1f, 0); 
+        yield return new WaitForSecondsRealtime(waitTime);
+        Camera.transform.position += new Vector3(0, .2f, 0);
+        yield return new WaitForSecondsRealtime(waitTime);
+        Camera.transform.position += new Vector3(0, .3f, 0);
+        yield return new WaitForSecondsRealtime(waitTime);
+        Camera.transform.position += new Vector3(0, -.3f, 0);
+        yield return new WaitForSecondsRealtime(waitTime);
+        Camera.transform.position += new Vector3(0, -.2f, 0);
+        yield return new WaitForSecondsRealtime(waitTime);
+        Camera.transform.position += new Vector3(0, -.1f, 0);
+        yield return new WaitForSecondsRealtime(waitTime);
+
+
+    }
+
+    IEnumerator rotate()
+    {
+        Camera.transform.eulerAngles = Vector3(0, 0, 0);
+        yield return new WaitForSecondsRealtime(waitTime);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
