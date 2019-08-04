663e49f80fa67332f409d1fb4258b74211333702
Henry Keyser <hwkeyser@gmail.com>
2019-08-03 21:46:16 -0400

    Commit Mousetracked Camera

---
+++ GMTK_2019/Assets/DetachEngine.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetachEngine : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Update()
+    {
+        this.transform.parent = null;
+        this.GetComponent<Rigidbody>().velocity = new Vector3(0, 0, this.GetComponentInParent<Controls>().startSpeed);
+    }
+
+
+
+}
